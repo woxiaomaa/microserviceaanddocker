0ea58a53e557d4e5668d9c311cf6d1b4141717e2
mh_desktop <1042880826@qq.com>
2019-03-14 17:55:16 +0800

    user-thrift-service实现将项目打包自动编译为docker镜像到远程linux服务器，详见pom文件的docker-maven-plugin 和/src/main/docker下的Dockerfile

---
+++ api-gateway-zuul/src/main/java/com/ma/filter/MyFilter.java
@@ -0,0 +1,49 @@
+package com.ma.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by mh on 2018/7/14.
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    /**
+     * 实现拦截器功能
+     *
+     * */
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        return null;
+    }
+}
--- userthriftservice/src/main/java/com/ma/UserThriftServiceApplication.java
+++ userthriftservice/src/main/java/com/ma/UserThriftServiceApplication.java
@@ -2,12 +2,20 @@ package com.ma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserThriftServiceApplication {
+public class UserThriftServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserThriftServiceApplication.class, args);
+	}
+
+	@Override//为了打包springboot项目
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 
 }
